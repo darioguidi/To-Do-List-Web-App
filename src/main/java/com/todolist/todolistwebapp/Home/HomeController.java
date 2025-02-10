450e6b20517538b245275595fa8e00a9ff8f940d
Dario Guidi <dario.guidi954@gmail.com>
2025-02-10 22:09:05 +0100

    .

--- src/main/java/com/todolist/todolistwebapp/Home/HomeController.java
+++ src/main/java/com/todolist/todolistwebapp/Home/HomeController.java
@@ -1,14 +1,13 @@
 package com.todolist.todolistwebapp.Home;
 
 import com.todolist.todolistwebapp.Nota.NotaService;
-import com.todolist.todolistwebapp.Nota.Note;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping(path="/home")
@@ -20,13 +19,6 @@ public class HomeController {
     public HomeController(NotaService notaService) {
         this.notaService = notaService;
     }
-/*
-    @GetMapping
-    public String loadHomePage(Model model){
-        List<Note> notes = notaService.getNotes();
-        model.addAttribute("notes", notes);
-        return "home";
-    }*/
 
     @GetMapping
     public String loadHomePage(){
